ef243091a13b8468dd4f4f3cd1fd198b14d3dd13
Guenta <pugadim123@gmail.com>
2026-07-07 16:51:05 +0100

    Configure deployment for personal AWS account

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cloud-final-tfstate-991346768340"
+    bucket         = "cloud-final-tfstate-631919638134"
     key            = "dev/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "cloud-final-tflock"
