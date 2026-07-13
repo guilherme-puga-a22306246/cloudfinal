39ec632b0b910fc5bab4645c60ed5d12213db1ca
Guenta <pugadim123@gmail.com>
2026-07-13 06:06:48 +0100

    health check on standby + automated promotion workflow

--- infrastructure/terraform/modules/route53/main.tf
+++ infrastructure/terraform/modules/route53/main.tf
@@ -30,15 +30,31 @@ resource "aws_route53_record" "primary" {
   }
 }
 
+resource "aws_route53_health_check" "standby" {
+  fqdn              = var.standby_alb_dns_name
+  port              = 80
+  type              = "HTTP"
+  resource_path     = "/actuator/health"
+  request_interval  = 30
+  failure_threshold = 3
+
+  tags = {
+    Name = "standby-alb-health-check"
+  }
+}
+
 resource "aws_route53_record" "standby" {
   zone_id = var.hosted_zone_id
   name    = var.domain_name
   type    = "A"
 
   set_identifier = "standby"
+
   failover_routing_policy {
     type = "SECONDARY"
   }
+
+  health_check_id = aws_route53_health_check.standby.id
 
   alias {
     name                   = var.standby_alb_dns_name
